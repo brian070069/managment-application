ef9dcb509ac517d69f1ad1d3b333b1895c0fa558
brian070069 <brianwgatundu@gmail.com>
2024-02-18 09:53:07 -0800

    style refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         14: "14px",
       },
       backgroundColor: {
-        "main-bg": "#FAFBFB",
+        "main-bg": "#20232A",
         "main-dark-bg": "#20232A",
         "secondary-dark-bg": "#33373E",
         "light-gray": "#F7F7F7",
